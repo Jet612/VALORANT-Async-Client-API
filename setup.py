fd3b2ef086d24a5f4650d37c8bcd8ab1b21169e2
JackAttack612 <jetflyer1130@gmail.com>
2023-01-10 14:47:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="valorantClientAPI",
-    version="0.0.99",
+    version="0.01.0",
     author="Jet612",
     description="Asynchronous Python wrapper for VALORANT's Client API ",
     long_description=long_desc,
